c09793f0fe1631fc17b91b188e95b2a387395076
renatofreitasRR <natogfreitas@gmail.com>
2024-04-10 19:28:59 -0300

    fork-commit

--- CharCountFunction/function_app.py
+++ CharCountFunction/function_app.py
@@ -8,6 +8,9 @@ app = func.FunctionApp()
 @app.route(route="CharCounter", auth_level=func.AuthLevel.ANONYMOUS)
 def CharCounter(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    
+    
+    # print("Forkando")
 
     name = req.params.get('name')
     if not name:
@@ -17,6 +20,7 @@ def CharCounter(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+        
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
